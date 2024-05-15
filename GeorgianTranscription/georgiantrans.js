0be0b23b1740354be5315189ef66c306a29ec6a3
jackisaacrabinovitch <84100302+jackisaacrabinovitch@users.noreply.github.com>
2024-05-15 12:38:14 -0400

    Add files via upload

--- GeorgianTranscription/georgiantrans.js
+++ GeorgianTranscription/georgiantrans.js
@@ -6,7 +6,7 @@ let g2r = dict.map((x)=>{return { search : new RegExp(x[0], "g") , replace: x[1]
 let r2g = dict.map((x)=>{return { search : new RegExp(x[1], "g") , replace: x[0]}})
 
 let translit = function(maparr){
-	let res = textarea.value
+	let res = textarea.value.toLowerCase()
 	maparr.forEach(it => {
 		res = res.replace(it.search,it.replace)
 	});
