61ff7d702bed83a725f9e6c5b03aab00c6e8a9ae
jackisaacrabinovitch <84100302+jackisaacrabinovitch@users.noreply.github.com>
2024-05-15 12:18:02 -0400

    Add files via upload

--- GeorgianTranscription/georgiantrans.js
+++ GeorgianTranscription/georgiantrans.js
@@ -1,26 +1,28 @@
-let dict = "ა,a;ი,i;უ,u;ე,e;ო,o;ქ,k;წ,c';ჭ,č';რ,r;ღ,ǧ;ტ,t';თ,t;ყ,q';პ,p';ს,s;შ,š;დ,d;ფ,p;გ,g;ჰ,h;ჯ,j;ჟ,ž;კ,k';ლ,l;ზ,z;ძ,dz;ც,c;ჩ,č;ვ,v;ბ,b;ნ,n;მ,m"
-    .split(";")
-    .map((x)=>{return x.split(",")})
+let dict = "ჭ,č';წ,c';ტ,t';ყ,q';პ,p';კ,k';ძ,dz;ღ,ǧ;შ,š;ჟ,ž;ჩ,č;ა,a;ი,i;უ,u;ე,e;ო,o;ქ,k;რ,r;თ,t;ს,s;დ,d;ფ,p;გ,g;ჰ,h;ჯ,j;ლ,l;ზ,z;ც,c;ვ,v;ბ,b;ნ,n;მ,m;ხ,x"
+	.split(";")
+	.map((x)=>{return x.split(",")})
 
-let map = {}
+let g2r = dict.map((x)=>{return { search : new RegExp(x[0], "g") , replace: x[1]}})
+let r2g = dict.map((x)=>{return { search : new RegExp(x[1], "g") , replace: x[0]}})
 
-dict.forEach(pair => {
-    map[pair[0]] = pair[1]
-});
-
-console.log(map)
+let translit = function(maparr){
+	let res = textarea.value
+	maparr.forEach(it => {
+		res = res.replace(it.search,it.replace)
+	});
+	response.innerText = res
+}
 
 let textarea = document.querySelector("textarea")
 let response = document.querySelector("div")
-let translit = function(){
-    response.innerText = textarea.value.split("").map(
-        function(x){
-            if (Object.keys(map).includes(x)){
-                return map[x]
-            } else {
-                return x
-            }
-        }
-    ).join("")
+
+let fliptext = function(){
+	let newresponse = textarea.value
+	let newtextarea = response.innerText
+	response.innerText = newresponse
+	textarea.value = newtextarea
 }
+
+
+
 
